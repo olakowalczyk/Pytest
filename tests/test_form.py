b9c2aa23b156a461d129b1c127a5d1c08a0f3a78
ola <olakowalczykola@gmail.com>
2023-03-12 00:09:22 +0100

    add docstring

--- tests/test_form.py
+++ tests/test_form.py
@@ -10,19 +10,22 @@ pytest_plugins = [
 
 class TestSubmitForm:
 
-    def test_initial_values(self, form_page):  # checks initial values
+    def test_initial_values(self, form_page: FormPage):  
+        """ Checks initial values """
         assert form_page.firstname_input_value() == 'Mickey'
         assert form_page.lastname_input_value() == 'Mouse'
 
 
-    def test_clear(self, form_page):  # checks clearing inputs
+    def test_clear(self, form_page: FormPage):
+        """ Checks clearing inputs """
         form_page.clear_form()
         assert form_page.firstname_input_value() == ''
         assert form_page.lastname_input_value() == ''
 
 
     @pytest.mark.parametrize("firstname, lastname", [("Donald", "Duck")])
-    def test_inputs(self, form_page, firstname, lastname):  # checks new input values
+    def test_inputs(self, form_page: FormPage, firstname, lastname):
+        """ Checks new input values """
         form_page.enter_firstname(firstname)
         form_page.enter_lastname(lastname)
         assert form_page.firstname_input_value() == firstname
@@ -30,7 +33,8 @@ class TestSubmitForm:
 
 
     @pytest.mark.parametrize("firstname, lastname", [("Peter", "Pan")])
-    def test_submit(self, form_page, firstname, lastname):  # checks sumbit i.e. reload page
+    def test_submit(self, form_page: FormPage, firstname, lastname):
+        """ Checks sumbit i.e. reload page """
         form_page.enter_firstname(firstname)
         form_page.enter_lastname(lastname)
         form_page.submit()
